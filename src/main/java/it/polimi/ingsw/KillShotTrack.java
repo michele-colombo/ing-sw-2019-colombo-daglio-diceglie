de2527aea08d72a9a3f3ebb997ef0c1f662275c9
michele-colombo <michele11.colombo@mail.polimi.it>
2019-04-17 08:37:59 +0200

    javadoc added to KillshotTrack

--- src/main/java/it/polimi/ingsw/KillShotTrack.java
+++ src/main/java/it/polimi/ingsw/KillShotTrack.java
@@ -6,12 +6,31 @@ import java.util.List;
 import java.util.Map;
 
 public class KillShotTrack {
+    /**
+     * keeps the number of skulls currently remaining in the match
+     */
     private int skulls;
+    /**
+     * A support map used to keep track of the killings of each player
+     */
     private Map<Player, Integer> killingCounter;
+    /**
+     * A list to keep track of the order of killing
+     */
     private List<Player> killingOrder;
+    /**
+     * The actual killshoot track, each slot is a tuple (Player, number_of_drops)
+     */
     private List<Map<Player, Integer>> track;
+    /**
+     * A constant used to determine the points to give to each player
+     */
     private final int biggerScore;
 
+    /**
+     * Constructs a killshot track with the specified number of initial skulls.
+     * @param skulls Initial skulls
+     */
     public KillShotTrack(int skulls){
         this.skulls = skulls;
         killingCounter = new HashMap<>();
@@ -20,10 +39,21 @@ public class KillShotTrack {
         biggerScore = 8;
     }
 
+    /**
+     * Returns the ordinal position of killing of hte specified player
+     * @param p
+     * @return An integer representing the ordinal position. 0 is the first.
+     */
     public int getKillingOrder(Player p){
         return killingOrder.indexOf(p);
     }
 
+    /**
+     * Returns the number of drops of the specified player on the killshot track
+     * If a player overkills someone, it is counted twice
+     * @param p
+     * @return An integer representing the number of drops on the killshot track
+     */
     public int getKillingOf(Player p){
         return killingCounter.getOrDefault(p, 0);
     }
@@ -32,6 +62,11 @@ public class KillShotTrack {
         return skulls;
     }
 
+    /**
+     * Adds the drops corresponding to killing (eventually with overkill) to the killshot track.
+     * Support variables are updated, too.
+     * @param killed A tuple (player, number_of_drops_to_add)
+     */
     public void addKilled(Map<Player, Integer> killed){ //map is actually a tuple
         track.add(killed);
         int temp;
@@ -50,6 +85,10 @@ public class KillShotTrack {
         removeSkull();
     }
 
+    /**
+     * Returns the number of points to add to each player. Only players who killed at least once are in the list
+     * @return a map (player, points_to_add_to_the_player)
+     */
     public Map<Player, Integer> score(){
         int currPoints = biggerScore;
         Map<Player, Integer> result = new HashMap<>();
@@ -63,6 +102,11 @@ public class KillShotTrack {
         return result;
     }
 
+    /**
+     * A support method used to get the greatest killer (counting overkills) in a certain list of players
+     * @param killers The list of players to analyze
+     * @return the player who has killed most in the list
+     */
     private Player getGreatestKillerIn(Map<Player, Integer> killers){
         int max = -1;
         Player result = null;
@@ -79,6 +123,11 @@ public class KillShotTrack {
         return result;
     }
 
+    /**
+     * Support method used to get the succession of points to add to the players
+     * @param currentScore Points given to the last player scored
+     * @return Points to give to the next player to be scored
+     */
     private int nextScore(int currentScore){
         if(currentScore == 2 || currentScore == 1){
             return 1;
@@ -86,6 +135,10 @@ public class KillShotTrack {
         return currentScore - 2;
     }
 
+    /**
+     * Decreases the number of skulls on the killshot track
+     * @return True if the skull was the last
+     */
     private boolean removeSkull(){
         skulls--;
         if (skulls <= 0) {
