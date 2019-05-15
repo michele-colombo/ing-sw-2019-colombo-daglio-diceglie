1a68d74a650cb5e1f35c07f10f3ed78de789534e
DiceglieGiuseppe <10538525@polimi.it>
2019-05-15 15:57:28 +0200

    Giuseppe Diceglie modified ClientMain to get user input from command line, take clientConfig from file and ask from user otherwise

--- src/main/java/it/polimi/ingsw/client/ClientMain.java
+++ src/main/java/it/polimi/ingsw/client/ClientMain.java
@@ -1,46 +1,118 @@
 package it.polimi.ingsw.client;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class ClientMain {
+import static javafx.application.Application.launch;
+
+public class ClientMain{
+
+    static ClientConfig config;
+
+
 
 
     public static void main(String[] args) throws IOException {
-        boolean done= false;
+        config= new ClientConfig();
 
-        while(!done) {
-            System.out.println("Please, insert ip, port, connection technology (socket) and user interface(cli or gui):");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            Scanner intReader = new Scanner(System.in);
+        loadFromConfigurationFile();
+        getFromCmdArguments(args);
 
-            String ip = reader.readLine();
-            int port = intReader.nextInt();
-            String connection = reader.readLine();
-            String userInterface = reader.readLine();
+        while(! isValidIp(config.getIp())){
+            System.out.println("Please insert ip (X.X.X.X): ");
+            config.setIp(new Scanner(System.in).nextLine());
+        }
+        while(! isValidPort( config.getPort())){
+            System.out.println("Please insert port (from 1024 to 65535): ");
+            config.setPort(new Scanner(System.in).nextInt());
+        }
+        while(! isValidConnection(config.getLink())){
+            System.out.println("Please insert link type (socket or rmi): ");
+            config.setLink(new Scanner(System.in).nextLine());
+        }
+        while(! isValidInterface(config.getUserInterface() )){
+            System.out.println("Please insert user interface (cli or gui): ");
+            config.setUserInterface(new Scanner(System.in).nextLine());
+        }
 
-            connection = connection.toLowerCase();
-            userInterface = userInterface.toLowerCase();
 
-            if (isValidIp(ip) && isValidPort(port) && isValidConnection(connection) && isValidInterface(userInterface)) {
-                try {
-                    Client client = new Client(ip, port, connection, userInterface);
-                    client.startClient();
-                    done = true;
-                }
-                catch (IOException e){
-                    System.out.println("Error! Server non raggiungibile");
-                    return;
-                }
+        try {
+            Client client = new Client(config.getIp(), config.getPort(), config.getLink(), config.getUserInterface());
+            client.startClient();
+        }
+        catch (IOException e){
+            System.out.println("Error! Server non raggiungibile");
+            return;
+        }
+    }
+
+    private static void loadFromConfigurationFile(){
+        Gson gson= new Gson();
+        JsonParser jp= new JsonParser();
+
+        InputStream url= new ClientMain().getClass().getClassLoader().getResourceAsStream("clientConfig.json");
+
+        if(url==null){
+            System.out.println("No configuration file!");
+            return;
+        }
+        String json;
+        Scanner sc = new Scanner(url);
+
+        json = sc.nextLine();
+        sc.close();
+        config= gson.fromJson(json, ClientConfig.class);
+
+
+    }
+
+    private static void getFromCmdArguments(String[] args){
+        for(int i=0; i<args.length; i++){
+            String argument= args[i];
+            String nextArgument;
+            if(i< args.length - 1){
+                nextArgument= args[i+1];
+            }
+            else{
+                nextArgument= "";
             }
 
+            switch(argument){
+                case "-ip":
+
+                    config.setIp(nextArgument);
+
+                    break;
+                case "-port":
+
+                    config.setPort(Integer.parseInt(nextArgument));
+
+                    break;
+                case "-link":
+
+                    config.setLink(nextArgument);
+
+                    break;
+                case "-ui":
+
+                    config.setUserInterface(nextArgument);
+
+                    break;
+                default:
+                    break;
+
+            }
         }
     }
 
@@ -84,4 +156,53 @@ public class ClientMain {
         return true;
     }
 
+
+    private static class ClientConfig{
+        private String ip;
+        private int port;
+        private String link;
+        private String userInterface;
+
+        public ClientConfig() {
+            ip= "non-initialized";
+            port= -1;
+            link="non-initialized";
+            userInterface= "non-initialized";
+        }
+
+        public void setIp(String ip) {
+            this.ip = ip;
+        }
+
+        public void setPort(int port) {
+            this.port = port;
+        }
+
+        public void setLink(String link) {
+            this.link = link;
+        }
+
+        public void setUserInterface(String userInterface) {
+            this.userInterface = userInterface;
+        }
+
+        public String getIp() {
+            return ip;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        public String getLink() {
+            return link;
+        }
+
+        public String getUserInterface() {
+            return userInterface;
+        }
+
+    }
+
 }
+
