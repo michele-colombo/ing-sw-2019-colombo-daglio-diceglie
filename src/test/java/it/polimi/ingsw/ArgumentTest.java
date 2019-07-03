8d04be7b4b4ab7958c0e4d904a531f76137bbd8a
DiceglieGiuseppe <10538525@polimi.it>
2019-07-03 23:57:17 +0200

    Added some javadoc and removed hardcoded things

---
+++ src/test/java/it/polimi/ingsw/ArgumentTest.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.server.ArgumentNavigator;
+import org.junit.jupiter.api.Test;
+
+public class ArgumentTest {
+
+    public static final String MERDA = "merda";
+
+    @Test
+    public void funziona(){
+        ArgumentNavigator a= new ArgumentNavigator(new String[]{"-io", "sono", "-tu", "sei", "-egli", "e"} , "-");
+        System.out.println(a.getFieldAsStringorDefault("io", MERDA));
+        System.out.println(a.getFieldAsStringorDefault("tu", MERDA));
+        System.out.println(a.getFieldAsStringorDefault("egli", MERDA));
+
+        try{
+            System.out.println(a.getFieldAsIntOrDefault("tu", -1));
+        }
+        catch (NumberFormatException e){
+            System.out.println("help");
+        }
+
+        System.out.println(a.getFieldAsIntOrDefault("noi", 4));
+        System.out.println(a.getFieldAsStringorDefault("voi", MERDA));
+
+        ArgumentNavigator b= new ArgumentNavigator(new String[]{"-io", "sono", "-tu", "sei", "-egli", "e"}, "--");
+        System.out.println(b.getFieldAsStringorDefault("qualcosa", "nafing"));
+    }
+}
